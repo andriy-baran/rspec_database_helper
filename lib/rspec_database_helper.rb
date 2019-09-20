eb0a5d2f7bd4a8f983995ff09e1c1650616e3219
Andrii Baran <andriy.baran.v@gmail.com>
2019-09-20 11:17:41 +0300

    bundle gem

---
+++ lib/rspec_database_helper.rb
@@ -0,0 +1,6 @@
+require "rspec_database_helper/version"
+
+module RspecDatabaseHelper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rspec_database_helper/version.rb
@@ -0,0 +1,3 @@
+module RspecDatabaseHelper
+  VERSION = "0.1.0"
+end
